dfac85792b2278722bfdd1c9a645e12c0d547e5c
Kenny Dasilva <kennydasilva542@gmail.com>
2026-01-24 21:51:07 +0200

    addiction of cache 15 minutes stored

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+
+#cache
+@cache_page(60*15)
+def property_list(request):
+    """ the View that returns all properties.
+    the response is stored in the cache on redis for 15 minutes."""
+
+    properties= Property.objects.all()
+    return render(request, "properties/property_list.html", {"properties": properties})
