07d1a7554a8991917004978cea27d28b8f8cee3f
Kenny Dasilva <kennydasilva542@gmail.com>
2026-01-24 22:57:30 +0200

    addiction of complete task of  cache

--- properties/views.py
+++ properties/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
+from .utils import get_all_properties
 
 @cache_page(60 * 15)
 def property_list(request):
@@ -8,6 +9,6 @@ def property_list(request):
     Returns all properties as JSON.
     Response is cached in Redis for 15 minutes.
     """
-    properties = Property.objects.all().values('id', 'title', 'description', 'price', 'location', 'created_at')
+    properties = get_all_properties
     data = list(properties)
     return JsonResponse({"data": data})
