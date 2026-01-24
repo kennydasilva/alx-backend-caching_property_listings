dd310c180364a8cd86fdcc6eafe9885a97780b4c
Kenny Dasilva <kennydasilva542@gmail.com>
2026-01-24 22:11:31 +0200

    update 2

--- properties/views.py
+++ properties/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cachepage
 from .models import Property
 
 
 #cache
-@cache_page(60*15)
+@cachepage(60*15)
 def property_list(request):
     """ the View that returns all properties.
     the response is stored in the cache on redis for 15 minutes."""
